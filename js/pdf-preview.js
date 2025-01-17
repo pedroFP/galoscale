a9fa4d270372922b3bacd658ffcd842818285b6d
PEDRO <pedro.araya.labra@gmail.com>
2025-01-17 15:10:04 -0300

    add loaders to PDF sectinos

--- js/pdf-preview.js
+++ js/pdf-preview.js
@@ -35,6 +35,7 @@ document.querySelectorAll(".pdf-file").forEach((container) => {
   const url = container.getAttribute("data-url"); // Get the PDF URL
   if (url) {
     renderPDF(url, container); // Call the render function
+    container.querySelector(".spinner-border").remove();
   } else {
     container.textContent = "No PDF URL provided.";
   }
